5b96619d89a2f68c35419cbfee85d80d8951cb3b
Lucas Neves <lucas54neves@gmail.com>
2026-05-18 21:23:10 -0300

    style: apply ruff formatting to existing code

---
+++ 01-python-foundations/exercises/lesson-01/temperature_converter.py
@@ -0,0 +1,22 @@
+def celsius_to_fahrenheit(celsius: float) -> float:
+    return celsius * 1.8 + 32
+
+
+def convert_user_input(user_input: str) -> float:
+    return float(user_input)
+
+
+def user_input() -> float:
+    return convert_user_input(
+        input("Insert a temperature in Celsius to convert to Fahrenheit: ")
+    )
+
+
+def main():
+    user_input_as_str = user_input()
+    user_input_as_float = convert_user_input(user_input_as_str)
+    temperature_as_fahrenheit = celsius_to_fahrenheit(user_input_as_float)
+    print(f"Temperature in Fahrenheit: {temperature_as_fahrenheit} F")
+
+
+main()
